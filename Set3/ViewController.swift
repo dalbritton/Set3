7d839429b07e2c537b4d8685f3e354703b2df94b
davida <davida@davids-macbook-pro.local>
2019-01-22 20:48:21 -0600

    Initial Commit

---
+++ Set3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set3
+//
+//  Created by davida on 1/22/19.
+//  Copyright © 2019 davida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
